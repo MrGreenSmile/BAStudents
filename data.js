b88b67206e9cae88ddd037b8373428feee97ddd6
GreenSmile <138634998+MrGreenSmile@users.noreply.github.com>
2026-04-08 18:43:17 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,59 @@
+let field_colm = ["https://i.namu.wiki/i/U4OXDv6L2TMZdcjEHxbIa0CZ8LNeVahOo33EnX382-Cb9Nrsad559zQohCkHUowxFUz-OG5np6Ho4Fv7Py2tCA.webp",
+	"https://i.namu.wiki/i/l08Nkpax2x-nWugiaLCbJrEus6MvKjwlt3Ru31fSzx1WrCkxUQ8H6ovMnLrvU6W3PBrq3kyvAUM_JJczNy1NYQ.webp",
+	"https://i.namu.wiki/i/89QRtm43IjlJPfYspHoSo0oBWpw3EkY5zP-i9-N99ijBBJuvlYfRmEbhzYyD_aHYKO4BuCBEjDV6pCMd3Wjicg.webp"
+	]
+let field_pref = {
+	"SS":"https://i.namu.wiki/i/FnArpI5CQtptIM0ul6SZLkkIhzoDF-oYLk2QB_V6jQb3POcW0NRV6Q15uXLwRHXo_gMs8l6Y2ddEZm214KP9gg.webp",
+	"S":"https://i.namu.wiki/i/t-dFhPc2rAlmVaFsuCnmTPNfVNpXWzircIhaMAO1CEY3RL8Kv-pDEiaFX-iaioMjoUFc3Eeigc4XbABikAOLcQ.webp",
+	"A":"https://i.namu.wiki/i/xo-iIcxJ2Xv3S1xcyZ8eRMv8uxwOmGCztbL67VT_VGoav6SOGIToKy2bCcBhHDtPweau1dUkkikJghq495xDAA.webp",
+	"B":"https://i.namu.wiki/i/jwrJhlI8A-Wqac9Li_XCBYGXJNYktJtJZEJbI4A4sQqqM8OjCZ2aj42Ur5z58LUTiN24WJYAqyot4sNwqwOXAA.webp",
+	"C":"https://i.namu.wiki/i/l2vc3Bt7Go_KSixFCSICNSJnBvYRcmBLADIGGzCaqpZ-DSB5EzXWJbJaRnDwDunBGxeXmtahzZ4rIgt3o7KGXw.webp",
+	"D":"https://i.namu.wiki/i/aJwhuU3kpVy-Kc1D0sKFtlrM0TrndrEvanjOXVK-wkqoqioeCSF2t3ffzUhZCPQ7GlEOoxln525iqw_aof3pbA.webp",
+}
+let skill_title = ['EX 스킬', '기본 스킬', '강화 스킬', '서브 스킬']
+let skill_books = {
+	"비의서":"https://i.namu.wiki/i/R7Rm47JQSVdqKNlpXsN5dI04MWibNfyZF5KuUAZFfk60-qVKVveEDbu0sU22weup0oMlw-DhjGOZ78pf0BwqbheJ3-rrJ8QYiVxebsNw4BZWiSzWIyvSaQLLcCydyAlESOW9MVEH0rAIWPokA0G8-w.webp",
+	"밀레니엄_DB":"https://i.namu.wiki/i/ZqFgME9Z1j6HfiPre-9nlA7pzDtlFbLQvPPoMsTVQcymf_cujCSLup0KHqFz6iGhk8I-bfLlx84_3LBsrkqlFQH2sOjxu70YSAciVxLwqDIHBlzxwyKWQx6Q9_Pf1iyTIblXewBBqcAWpp4VJr6FIw.webp",
+	"밀레니엄_Book":"https://i.namu.wiki/i/cnCynjnjdLfBzID1LtndhqKfolkwM6cMHHoVdRCDneCxWnL2eCc0EsXyC2R3gUqL4vydYK8KP4CAz2c9J_xQTLP_t2BVVF1YkYkTFH7bobJv7zply4WVnIH1qLuolgswNdlaHBrnfbJC0_BMMMJRvg.webp",
+	"아비도스_DB":"https://i.namu.wiki/i/mNZfFjmZjmMarR0Ia5dcH2Fv6DTRq6XlBXRg1iBVJc2IHmdmBLkf0NchWgsm1l75s0KbADpiNZ_bzSUTuG6ztS52r_wBoD5AMjzOPHS0EBvyRInrC5ziwmmaaWMMp7a1OHiqzIl8t9tH3Y3JIYm-sg.webp",
+	"아비도스_Book":"https://i.namu.wiki/i/BCufoD1I32X5fEBBgcBgZEHMkDnsIiLp8y3h9cnyeuV2r9DkTFoSGnJqD-wMpgBDXUtQiERjcbNSyEIDz9eFOz6kksnHFrnLp0pzv-1J4fWBaJr_fqxR11KZQqmZHtmI__SqMNZYZXVG2JJXfAAx1A.webp",
+}
+let skill_matterial = {
+	"디스코 콜간테":"https://i.namu.wiki/i/s_Viqyy1wZcMuEQy64nVYVCe-yUa4IF7mhDiKmSD1LITuFXQd_nCDDlJIZLlM0rILoTZPVgFyQ0x7fY5x1vz53eJSKoHtEubquNoudEFSipvVTo7zLQiBAl2BJG8DKxEGOAQWzgrWAV7KaHAFGWmxA.webp",
+	"수정 하니와":"https://i.namu.wiki/i/sQf5wqmytH0l6BiBFIZSAsABlZXrM0Dk0nk3iWbnydhfErUsv2rMmNYcscdb2RqliSw2y6NXtXJwMyEr13P-n2jahsxZ5xLdI7FmZloYCA_KVmHffd8pEzV7dRHvE2MVJPIIil3tGEvarU6QiW7geg.webp",
+	"파에스토스 원반":"https://i.namu.wiki/i/o-6S8WcLLh9n76P05r8n8a2hzgsj0wgkO5ZAmVvl-Pd6Wa5T5kUDVYDuDdmPU1RqtfhZ9spdccsNdA0_CnsLa0UmWQcAsSI9LVNyeFlk1WbcjQtjLx-FNlRlxfW6GQEDH2pU2vP0nc87VENNwMtNhA.webp",
+	"에테르":"https://i.namu.wiki/i/Fs8NIbyVIjDlkqFHI2BAFCa_R6YIZrq556sn6e3rwLpE4p4zYxJwU0gsXkLpuxwGpm7B1JHMdJkB_c1exslKHCWTd6gKTj-L8_mmdHKzAACvOcInoneNrS__r0z2JLN8ATBgbAsGn9HzjV2JY0m-rQ.webp",
+	"님루드 렌즈":"https://i.namu.wiki/i/XIZiR_yC509t2cAzbfliwDcuIcHRcJtI1mC_rO44uPBp23ZHDKdinCBx4LyGkqNol1M97yLyPc3bqJqcnaf4GMjtVWi_xIT6gtO9n7nbACTpvCOKPqraM2TAmtIvZ41TATtoeVGkrgUjwyOCpo0ppA.webp",
+	"안티키테라 장치":"https://i.namu.wiki/i/Z2LwE78i3nzQYr05mD0SVCwXvPuZc0i3sIj4RKTuRIMxvc9EJGBTiswUJSCMLABDe6upZpy32o8hV8NCVEfJc8gtAAOdcLpePhDQm43373KDhLVwSbppjP2wQ6QZBkbks0mUd2-k3ihkKTP1FwhSnA.webp",
+	"보이니치 사본":"https://i.namu.wiki/i/MOU-P9eNFRq53JzGUtpQcVJWnxuB7YjZ2QWecUM30tDO74iCsOapnwtM9gV5uCUixxQFeQ4rpZH5iAHXYjnZA5CqDllhnwrJ1cfRv9c5-qcRAWOEWBWrdNZVeoJI4zYd-nqPhprILGHJEUAsjH50lA.webp",
+	"위니페소키 스톤":"https://i.namu.wiki/i/An3u6iuKeVumhZDfmlPRXxe70WpkzW5Fx-GXeLm6Slt0uhPAODC0LR91oNJnZzEPHWJKs8SAFl4lpcHn7AtZKxgwQxpEpuDXd4VUXpjEReaftkz3_iw7S4ii4v3cZPKqdugd2WRSAYoqDU2kZnRagQ.webp",
+	"고대 전지":"https://i.namu.wiki/i/KfAU4edNTWmN7Kyk0cYCTmNtwaoCxOGqALXDl0UE32e_XnoowmoT6K2R_hFzpjZXHSLjSTWETgv8k24WwDvJWFcc-nAoyut85MEsR19_XVRN_yKO2fE4XM0hSJ55QdeNXWr77EMJVZU9IfRPPDUAnw.webp",
+	"머리가 자라는 인형":"https://i.namu.wiki/i/SHS7fGF2G6Y-eHSTmPpVNXwYVnzewYAfDaDqHaYzQCRV1oiZyGCcROWxWRF21opWTr2JhTbvOXfbmAFkysDoJWCtlFgNVVYFN7y7dsnCH1-9R3kYTMJiSi3zAYhvjT1Pj4qrSSY_SVmD7Pwedj-i7g.webp",
+	"만드라고라":"https://i.namu.wiki/i/SuXn2UQMhJfBUkUilBR08gAqY2cwDvk4rWIdY_GQ_uuXrheMdciePlLNnpdnQGVHUckuy71RGhnXOSM4JJhDm1fFltgt-qibb3ADWjA3n0n_3TXK0wwq9z6sXU1jXPAfAuYEyKvw6ha_TU_QpyZunQ.webp",
+	"네브라 디스크":"https://i.namu.wiki/i/D-Wt1teCQwkr9HkClxRGMM9fvW0MJ0V036NKwtIyCxzmRMDvVg8rrTWXi_xHb4FcFcf0oqYjPZb5Hwb3LUS9LQePhRwIZkB5sqmHu3QN-zA3G5-kp_btKpv0wcPDVmjb529LR3MwpCWT3neMjX4JXA.webp",
+}
+
+let weapon_thumb = {
+	"HG":"https://i.namu.wiki/i/DwtX_Q0n0XWNr1KbYm4zhWSs8S7LW1iIMoUhF0AB-6ia2xiG0QD-74U8ok6QNe5bbuHjE_7UcqAKo4eGF0oXHFLtjoCMB_N0Y1EpLDMQcOFOudEFBZLRO250Ftds8mH8XnC71K0wy-P835v5XW3dfQ.webp",
+	"SMG":"https://i.namu.wiki/i/9v5K-azHmcyMgyx39ogqwZJnM_KEJmYxd_DGE4eMp0TCBg4Zc_B7NGc5i8NaFG-AFm6SSCpG8lfL53Wq0QKKbw.webp",
+	"AR":"https://i.namu.wiki/i/IsEE0ctz3G0-mgv_GLj9pVyJV_r5CP3UIJ4uJKm4VvvCFnsSO3aXeRVuvTvwINr7IV1jaOOEX38O-oOpgHi-9w.webp",
+	"SR":"https://i.namu.wiki/i/191X7S0T7jDjhYRowqZw9k8s4ZyPh6ZFB6f6aA3aZL7aGMq4H41LBMppOvv7rWrjy72s6ZG0UivR8cYWux73fg.webp",
+	"SG":"https://i.namu.wiki/i/uU0KIt-4ts36zhluAh7U8dwMDV_Lv0oI4Fgbsld8JfJti4uDfwTqa7UO3TtgYRoRwZgPmLpZfgabKhV_rCys3Q.webp",
+	"MG":"https://i.namu.wiki/i/z8Q_Vue3BUJG7O5wZCq_PaFr-vm91Lo269G9PHfgNqVVCvSGIRcl5qWXrk_JFiXzUPpzOshoLMj61OtLT-ldNA.webp",
+	"GL":"https://i.namu.wiki/i/VAF5dwWskK_YB8SPP8hXGeJxQj8CiqsJxeDrdJ1FWTexSy4AdNKTvlhvFKBC7bCpMGQWkVH0akyKXXrIlDUqlg.webp",
+	"RL":"https://i.namu.wiki/i/bIgQR0UDREoYx9RZH37SiASiLN6O1yn5whFA8XwE5fgrXZ_72Y0JNBr7ntJZhwlBQiYzT2fikrHp11J68vlFfiuLRK7bKbw31bpov2NNnjSF6Yg9SWicQBKXFeGHKvAIWsidTPAfpkLYedcLELZSyQ.webp",
+	"RG":"https://i.namu.wiki/i/MgJb-bieY3WxWDSBzNklKDYKxMO3EsTg25kzvrgiGVWTtYpBtbvwtHa66CC1b7QlqnQgZaJYiBmJCLIaYZnlEA.webp",
+	"MT":"https://i.namu.wiki/i/iJJGNfA5Ru5NCkBGm4tIXv0KBexLoJQftyFdROwYpsc-K-iltoFb8zNGPATdrwGk8A33x2k9Q6CNWcH0QzQwfw.webp",
+	"FT":"https://i.namu.wiki/i/gNKchwcoOx9kdXSj5N9Ci63-qwjOFOY4grxP0Xwi7I2ps-UW4aP-f1EjG_tifGA8V7isv3ID28G4f1F2b_BMggjW7mB5-PXBjwdgbzPBNJUvj9wkTPVpPqh5aq85IDkYNttYwRVEFecR0d3ClBmaKw.webp"
+}
+let equipment_icon = {
+	"신발":"https://i.namu.wiki/i/Rowh7tqI0LtdPYUKTj7_zsbu_o69UZorrjkyAy_0UTtQgOI8pgCXkIP5v9Qc9Yf5T1F8IeXbwRlAkBBMoMQV7ZeqBy7yD4m5HrwBc3u3brKcPuJ7FuYZ6cT73S0lBvhKakh_x61Zmm07ZjtKzq7JOw.webp",
+	"헤어핀":"https://i.namu.wiki/i/hHFvEOr1vTFf3CN81l8yRzIoY3hotNhJhLC8mVUinNzFHiIJXa7HYvybm_KIfkgYO-gF4FbPeeqseqQTH2x-Qw.webp",
+	"가방":"https://i.namu.wiki/i/tHTUkAlta3GBpLSVzXZMvsEeMiF-epGrQDX7MM7P7BAvYJYlixFUnhPDl7DmdhrLJT41tlyP_OZFq9Hi5q0klw.webp",
+	"손목시계":"https://i.namu.wiki/i/WEJbzPb5fmc3rC9sPnPmIe6lt9g_cab1rY3cxsBKP-oIotnmmTHp4IhMAzeG63cJZblJU0owu4sBOdvk-e-Ba-OfmqWrDB_gIZmmwJmElISXtGf2Ie7f4N1TczyIrJrseRxJwCpelYIfbuCTwM1kcA.webp",
+	"배지":"https://i.namu.wiki/i/swiB0C6jlVOO3KjOMtwpqiosumaGhsgew1FMDEvFKmmF7EkENu-hYUsM7UQGleWBZMbY1l-mfSAukhzxQc0RIw.webp",
+	"부적":"https://i.namu.wiki/i/vkKoydPExbywxy5sdI9gmML3xmXjxKe6dKDVj4QQinlnUSHMt9j8Gx1daikHlqTsi-Ik7gPQIhsz7G-Ai7EIDbCoqlLgQQIeWxRt6mcXC07uZako9Z9jOujn0TWlNpSBvXKX0jClPGmjjp3LBWM0mA.webp",
+	"목걸이":"https://i.namu.wiki/i/yAtV8olkmZNSikoxMtGrFSKfcLgGpREWrVJrIm7b3s9SIdRYxa1jpDn8viDY5RXa2XPIjhfSMgt6zDKiqbZubA.webp",
+	"장갑":"https://i.namu.wiki/i/lzJ9RuOXIouKoltlvMsq4C7W6lGO4sJ2Ej0g2tCjh1qT7QeKAzcoAoPg2OIzsNB_7oviPrRgV1S3pxiJB-lS5A.webp",
+	"모자":"https://i.namu.wiki/i/XURyknNY03D1oGX8SDF4KB2j8h2-e1VxqVC5O8CmvS4HOsluX9nJZSq0POM8MZigrJ6JqTwtfrkDe3daDL2khR_c1XVaiW6I1ghXDufhzvcRWhauTRlRsiNmvZXNRsiHkH57HEgNJEWKw_Ami22uAA.webp",
+}
